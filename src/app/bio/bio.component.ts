34de69e021f7c3c7c12dd924fe703abd41b90679
Philip Fulgham <philfulgham@gmail.com>
2017-03-04 21:41:27 -0500

    Improved white space in code and updated to Angular 2.4.6

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -2,31 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../theme.service';
 
 @Component({
-  selector: 'app-bio',
-  templateUrl: './bio.component.html',
-  styleUrls: ['./bio.component.less']
+    selector: 'app-bio',
+    templateUrl: './bio.component.html',
+    styleUrls: ['./bio.component.less']
 })
 export class BioComponent implements OnInit {
     
     favoriteGames = ["Dishonored", "Minecraft", "Assassin's Creed", "The Elder Scrolls", "Batman: Arkham"];
     
-favoriteShows = ["Doctor Who", "The Flash", "Gotham", "The Big Bang Theory"];
+    favoriteShows = ["Doctor Who", "The Flash", "Gotham", "The Big Bang Theory"];
 
-  constructor(private themeService: ThemeService) { }
-    
-public show(list) {
-    //document.getElementById(list).style.display = "block";
-}
-    
-public hide(list) {
-    //setTimeout(function() { document.getElementById(list).style.display = "none"; }, 500);
-}
+    constructor(private themeService: ThemeService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        
+    }
     
     ngAfterViewInit() {
        window.scrollTo(0, 0);
-   }
+    }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../theme.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
 
     bio;
     skills;
     work;
-constructor(private themeService: ThemeService) { }
 
-  ngOnInit() {
-  }
+    constructor(private themeService: ThemeService) { }
+
+    ngOnInit() {
+        
+    }
 
 }
