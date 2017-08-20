db8fbaa35112d35a2545ba7515e97f7dda184568
Philip Fulgham <philfulgham@gmail.com>
2017-08-20 18:43:29 -0400

    feat: add GitHub widget, job history, edu history

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ProjectsComponent implements OnInit {
 
-	constructor() { }
+	jobs;
 
-	ngOnInit() {
+	constructor() {
+		var jobs = require("../../assets/jobs.json");
+		this.jobs = jobs;
+		jobs.sort((a, b) => {
+			if (a.start > b.start) {
+				return -1;
+			}
+			if (a.start < b.start) {
+				return 1;
+			}
+		});
 	}
+
+	ngOnInit() {}
 
 }
