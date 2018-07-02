f8824f3b866d2e13ca2ed91049365222ab94c402
Philip Fulgham <philfulgham@gmail.com>
2018-07-02 19:19:01 -0400

    Wire up AngularFire2 modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+
 import { ResponsiveModule } from "ng2-responsive";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireStorageModule } from "angularfire2/storage";
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { MainModule } from "main/main.module";
+
+import { environment } from "../environments/environment";
 
 @NgModule({
 	declarations: [AppComponent],
@@ -14,6 +20,9 @@ import { MainModule } from "main/main.module";
 		AppRoutingModule,
 		ResponsiveModule,
 		MainModule,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireStorageModule,
+		AngularFirestoreModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
