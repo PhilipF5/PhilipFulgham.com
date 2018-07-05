68d0b1fadfad9ba7f48264c27041e05a7286b476
Philip Fulgham <philfulgham@gmail.com>
2018-07-05 16:46:18 -0400

    Use Firebase Storage to load portfolio slides

--- src/main/components/portfolio/portfolio.component.ts
+++ src/main/components/portfolio/portfolio.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 
+import { AngularFireStorage } from "angularfire2/storage";
+import { Observable } from "rxjs";
+
 @Component({
 	selector: "portfolio",
 	templateUrl: "./portfolio.component.html",
 	styleUrls: ["./portfolio.component.scss"]
 })
 export class PortfolioComponent implements OnInit {
+	public photoUrls: Observable<string | null>[] = [];
 
-	constructor() { }
+	constructor(private storage: AngularFireStorage) {
+	}
 
 	ngOnInit() {
-		
+		for (let i = 0; i < 7; i++) {
+			this.photoUrls.push(this.storage.ref(`portfolio/${i + 1}.png`).getDownloadURL());
+		}
 	}
-
-	ngAfterViewInit() {
-		window.scrollTo(0, 0);
-	}
-
 }
