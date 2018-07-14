89227232f2aa545dd2a40f3338991640efed14b5
Philip Fulgham <philfulgham@gmail.com>
2018-07-14 10:36:02 -0400

    Use GreenSock for quote animations on intro screen

--- src/main/components/intro-screen/intro-screen.component.ts
+++ src/main/components/intro-screen/intro-screen.component.ts
@@ -1,13 +1,13 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
-import { TimelineLite } from "gsap";
+import { Power1, TimelineLite } from "gsap";
 
 @Component({
 	selector: "intro-screen",
 	templateUrl: "./intro-screen.component.html",
 	styleUrls: ["./intro-screen.component.scss"]
 })
-export class IntroScreenComponent implements OnInit {
+export class IntroScreenComponent {
 	@ViewChild("byline") private _byline: ElementRef;
 	@ViewChild("quote") private _quote: ElementRef;
 	@ViewChild("scrollArrow") private _scrollArrow: ElementRef;
@@ -24,7 +24,10 @@ export class IntroScreenComponent implements OnInit {
 		return this._scrollArrow.nativeElement;
 	}
 
-	ngOnInit() {
-		
+	public load(): TimelineLite {
+		return new TimelineLite()
+			.from(this.quote, 2, { x: -50, ease: Power1.easeInOut }, "start")
+			.to(this.quote, 2, { opacity: 1, ease: Power1.easeInOut }, "start")
+			.to(this.byline, 2, { opacity: 1, ease: Power1.easeInOut });
 	}
 }
--- src/main/pages/home/home.page.ts
+++ src/main/pages/home/home.page.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+
+import { TimelineLite } from "gsap";
+
+import { IntroScreenComponent } from "main/components";
 
 @Component({
 	selector: "home-page",
 	templateUrl: "./home.page.html",
 	styleUrls: ["./home.page.scss"]
 })
-export class HomePage {}
+export class HomePage implements OnInit {
+	@ViewChild(IntroScreenComponent) private introScreen: IntroScreenComponent;
+
+	ngOnInit() {
+		this.introScreen.load().delay(2);
+	}
+}
