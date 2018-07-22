396131a90200d1751c4a07917a55acbe40749a3c
Philip Fulgham <philfulgham@gmail.com>
2018-07-21 20:57:06 -0400

    Load background image with Firebase and GreenSock

--- src/main/pages/home/home.page.ts
+++ src/main/pages/home/home.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
-import { TimelineLite } from "gsap";
+import { AngularFireStorage } from "angularfire2/storage";
+import { Power1 } from "gsap";
+import { Observable } from "rxjs";
 
 import { IntroScreenComponent } from "main/components";
 
@@ -10,9 +12,19 @@ import { IntroScreenComponent } from "main/components";
 	styleUrls: ["./home.page.scss"]
 })
 export class HomePage implements OnInit {
+	public imageUrl: Observable<string | null>;
+
+	@ViewChild("background") private _background: ElementRef;
 	@ViewChild(IntroScreenComponent) private introScreen: IntroScreenComponent;
 
+	private get background(): HTMLElement {
+		return this._background.nativeElement;
+	}
+
+	constructor(private storage: AngularFireStorage) {}
+
 	ngOnInit() {
-		this.introScreen.load().delay(2);
+		this.imageUrl = this.storage.ref("galaxy.jpg").getDownloadURL();
+		this.introScreen.load().to(this.background, 6, { opacity: 1, ease: Power1.easeInOut }, 2).delay(2);
 	}
 }
