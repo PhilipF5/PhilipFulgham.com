d308c8461771381ade4913f4afec4ca0a8253c35
Philip Fulgham <philfulgham@gmail.com>
2018-07-27 20:35:26 -0400

    Load bio paragraphs from Firebase

--- src/main/components/about-me/about-me.component.ts
+++ src/main/components/about-me/about-me.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 
+import { AngularFirestore } from "angularfire2/firestore";
 import { AngularFireStorage } from "angularfire2/storage";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 import { PageSectionComponent } from "main/components/page-section/page-section.component";
 import { CollapsibleDirective } from "main/directives";
@@ -9,19 +11,24 @@ import { CollapsibleDirective } from "main/directives";
 @Component({
 	selector: "about-me",
 	templateUrl: "./about-me.component.html",
-	styleUrls: ["./about-me.component.scss"]
+	styleUrls: ["./about-me.component.scss"],
 })
 export class AboutMeComponent extends PageSectionComponent implements OnInit {
+	public paragraphs: Observable<string[] | null>;
 	public photoUrl: Observable<string | null>;
 
 	@ViewChild(CollapsibleDirective) private subsection: CollapsibleDirective;
 
-	constructor(private storage: AngularFireStorage) {
+	constructor(private afs: AngularFirestore, private storage: AngularFireStorage) {
 		super();
 	}
 
 	ngOnInit() {
 		this.photoUrl = this.storage.ref("portrait.png").getDownloadURL();
+		this.paragraphs = this.afs
+			.doc("text/pJwwIKKFUtJvxDiaWtxJ")
+			.valueChanges()
+			.pipe(map<any, string[]>(text => text.contents));
 	}
 
 	public toggleFavorites(): void {
