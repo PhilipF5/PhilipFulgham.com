b07b6bd31bbd67cb0a89bba75bd12df8d5b68ad7
Philip Fulgham <philfulgham@gmail.com>
2018-10-27 17:48:15 -0400

    Replace repo language text with icon

--- src/main/components/github-card/github-card.component.ts
+++ src/main/components/github-card/github-card.component.ts
@@ -29,16 +29,42 @@ export class GitHubCardComponent {
 		return this.http.get<T>(url).toPromise();
 	}
 
+	private getIconLink(icon: string) {
+		return `assets/icons/file_type_${icon}.svg`;
+	}
+
 	private async getRepos() {
+		let repos = (await this.get<any[]>(this.userUrl + "/repos")).sort((a, b) => {
 			if (a.pushed_at > b.pushed_at) {
 				return -1;
 			} else {
 				return 1;
 			}
 		}).slice(0, 5);
+
+		this.repos = [];
+		for (let r of repos) {
+			let repo = await this.http.get<any>(r.url, { headers: { "Accept": "application/vnd.github.mercy-preview+json" }}).toPromise();
+			if (repo.topics.includes("angular")) {
+				repo.icon = Icons["Angular"];
+			} else if (repo.topics.includes("react")) {
+				repo.icon = Icons["React"];
+			} else {
+				repo.icon = Icons[repo.language];
+			}
+			this.repos.push(repo);
+		}
 	}
 
 	private async getUser() {
 		this.user = await this.get(this.userUrl);
 	}
 }
+
+const Icons = {
+	"Angular": "angular",
+	"C#": "csharp",
+	"JavaScript": "js_official",
+	"React": "reactjs",
+	"TypeScript": "typescript_official",
+};
