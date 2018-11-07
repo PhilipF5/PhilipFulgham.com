1f0bfea5601461553d768aed8731ca305076e6fa
Philip Fulgham <philfulgham@gmail.com>
2018-11-06 21:57:09 -0500

    Remove old inheritance of page section component

--- src/app/main/components/projects/projects.component.ts
+++ src/app/main/components/projects/projects.component.ts
@@ -1,10 +1,8 @@
 import { Component } from "@angular/core";
 
-import { PageSectionComponent } from "app/main/components/page-section/page-section.component";
-
 @Component({
 	selector: "projects",
 	templateUrl: "./projects.component.html",
 	styleUrls: ["./projects.component.scss"],
 })
-export class ProjectsComponent extends PageSectionComponent {}
+export class ProjectsComponent {}
