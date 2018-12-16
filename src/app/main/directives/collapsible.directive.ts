1d2306aa664139a5c35d517eef40ce7a88c84e8d
Philip Fulgham <philfulgham@gmail.com>
2018-12-15 20:55:24 -0500

    Refactor collapsible section anims as timelines

--- src/app/main/directives/collapsible.directive.ts
+++ src/app/main/directives/collapsible.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, OnInit } from "@angular/core";
 
-import { Power1, TweenMax } from "gsap";
+import { Power1, TimelineLite } from "gsap";
 
 @Directive({
 	selector: "[collapsible]",
@@ -27,29 +27,30 @@ export class CollapsibleDirective implements OnInit {
 		this.hideInstantly();
 	}
 
-	public hide(): TweenMax {
+	public hide(): TimelineLite {
 		this.hidden = true;
-		return TweenMax.to(this.elem, 2, this.hiddenStyle);
+		return new TimelineLite().to(this.elem, 2, this.hiddenStyle);
 	}
 
-	public hideInstantly(): TweenMax {
+	public hideInstantly(): TimelineLite {
 		this.hidden = true;
-		return TweenMax.set(this.elem, this.hiddenStyle);
+		return new TimelineLite().set(this.elem, this.hiddenStyle);
 	}
 
-	public show(): TweenMax {
+	public show(): TimelineLite {
 		this.hidden = false;
-		TweenMax.set(this.elem, { height: "auto" });
-		TweenMax.from(this.elem, 2, { height: 0, ease: Power1.easeInOut });
-		return TweenMax.to(this.elem, 2, this.shownStyle);
+		return new TimelineLite()
+			.set(this.elem, { height: "auto" }, "start")
+			.from(this.elem, 2, { height: 0, ease: Power1.easeInOut }, "start")
+			.to(this.elem, 2, this.shownStyle, "start");
 	}
 
-	public showInstantly(): TweenMax {
+	public showInstantly(): TimelineLite {
 		this.hidden = false;
-		return TweenMax.set(this.elem, this.shownStyle);
+		return new TimelineLite().set(this.elem, this.shownStyle);
 	}
 
-	public toggle(): TweenMax {
+	public toggle(): TimelineLite {
 		return this.hidden ? this.show() : this.hide();
 	}
 }
