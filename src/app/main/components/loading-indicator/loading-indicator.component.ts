f0c9b142891c23db5f0ce6689dcb92d4515d5f60
Philip Fulgham <philfulgham@gmail.com>
2018-12-21 21:26:08 -0500

    Create loader component

---
+++ src/app/main/components/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+
+import { Linear, TweenMax } from "gsap";
+
+@Component({
+	selector: "loading-indicator",
+	templateUrl: "./loading-indicator.component.html",
+	styleUrls: ["./loading-indicator.component.scss"]
+})
+export class LoadingIndicatorComponent implements AfterViewInit {
+	@ViewChild("back") private _back: ElementRef;
+	@ViewChild("front") private _front: ElementRef;
+
+	private get back(): HTMLElement {
+		return this._back.nativeElement;
+	}
+
+	private get front(): HTMLElement {
+		return this._front.nativeElement;
+	}
+
+	ngAfterViewInit() {
+		TweenMax.set(this.front, { x: 1 });
+		TweenMax.to(
+			this.front,
+			3,
+			{ rotationZ: 360, transformOrigin: "11.5px 12.5px", ease: Linear.easeInOut },
+		).repeat(-1);
+	}
+}
