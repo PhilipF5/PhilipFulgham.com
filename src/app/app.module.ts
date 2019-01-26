e53a3a590e170e8b475912e7faaa1bfdaaa2770a
Philip Fulgham <philfulgham@gmail.com>
2019-01-25 22:40:51 -0500

    Remove Firebase dependency and deployment scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { ResponsiveModule } from "ng2-responsive";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireStorageModule } from "@angular/fire/storage";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { MainModule } from "app/main/main.module";
-
-import { environment } from "../environments/environment";
 
 @NgModule({
 	declarations: [AppComponent],
@@ -21,9 +16,6 @@ import { environment } from "../environments/environment";
 		AppRoutingModule,
 		ResponsiveModule,
 		MainModule,
-		AngularFireModule.initializeApp(environment.firebase),
-		AngularFireStorageModule,
-		AngularFirestoreModule,
 		HttpClientModule,
 	],
 	providers: [],
