531f9f01fab7a5fc28d65e1e9c69132783964583
Philip Fulgham <philfulgham@gmail.com>
2019-02-17 10:40:29 -0500

    Rename existing 'projects' section to 'activity'

--- src/app/main/components/index.ts
+++ src/app/main/components/index.ts
@@ -1,4 +1,5 @@
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { ActivityComponent } from "./activity/activity.component";
 import { ArrowComponent } from "./arrow/arrow.component";
 import { FavoritesListComponent } from "./favorites-list/favorites-list.component";
 import { GitHubProfileComponent } from "./github-profile/github-profile.component";
@@ -7,7 +8,6 @@ import { IntroScreenComponent } from "./intro-screen/intro-screen.component";
 import { LoadingIndicatorComponent } from "./loading-indicator/loading-indicator.component";
 import { MediumFeedComponent } from "./medium-feed/medium-feed.component";
 import { PageSectionComponent } from "./page-section/page-section.component";
-import { ProjectsComponent } from "./projects/projects.component";
 import { ResumePreviewComponent } from "./resume-preview/resume-preview.component";
 import { SectionTitleComponent } from "./section-title/section-title.component";
 import { ShadowButtonComponent } from "./shadow-button/shadow-button.component";
@@ -17,6 +17,7 @@ import { SiteNavComponent } from "./site-nav/site-nav.component";
 import { SkillsGridComponent } from "./skills-grid/skills-grid.component";
 
 export { AboutMeComponent } from "./about-me/about-me.component";
+export { ActivityComponent } from "./activity/activity.component";
 export { ArrowComponent } from "./arrow/arrow.component";
 export { FavoritesListComponent } from "./favorites-list/favorites-list.component";
 export { GitHubProfileComponent } from "./github-profile/github-profile.component";
@@ -25,7 +26,6 @@ export { IntroScreenComponent } from "./intro-screen/intro-screen.component";
 export { LoadingIndicatorComponent } from "./loading-indicator/loading-indicator.component";
 export { MediumFeedComponent } from "./medium-feed/medium-feed.component";
 export { PageSectionComponent } from "./page-section/page-section.component";
-export { ProjectsComponent } from "./projects/projects.component";
 export { ResumePreviewComponent } from "./resume-preview/resume-preview.component";
 export { SectionTitleComponent } from "./section-title/section-title.component";
 export { ShadowButtonComponent } from "./shadow-button/shadow-button.component";
@@ -36,6 +36,7 @@ export { SkillsGridComponent } from "./skills-grid/skills-grid.component";
 
 export const COMPONENTS = [
 	AboutMeComponent,
+	ActivityComponent,
 	ArrowComponent,
 	FavoritesListComponent,
 	GitHubProfileComponent,
@@ -44,7 +45,6 @@ export const COMPONENTS = [
 	LoadingIndicatorComponent,
 	MediumFeedComponent,
 	PageSectionComponent,
-	ProjectsComponent,
 	ResumePreviewComponent,
 	SectionTitleComponent,
 	ShadowButtonComponent,
