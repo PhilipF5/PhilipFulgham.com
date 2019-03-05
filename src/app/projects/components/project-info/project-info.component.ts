cb912f0aaeafbbd5ff29a03494779fd6efb2906d
Philip Fulgham <philfulgham@gmail.com>
2019-03-04 20:47:35 -0500

    Add transition animation for project info

--- src/app/projects/components/project-info/project-info.component.ts
+++ src/app/projects/components/project-info/project-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, NgZone } from "@angular/core";
+
+import { TimelineLite } from "gsap";
 
 import { Project } from "app/projects/models";
 
@@ -8,9 +10,26 @@ import { Project } from "app/projects/models";
 	styleUrls: ["./project-info.component.scss"],
 })
 export class ProjectInfoComponent {
-	@Input() selectedProject: Project;
+	private _selectedProject: Project;
+
+	public get selectedProject(): Project {
+		return this._selectedProject;
+	}
+
+	@Input() public set selectedProject(value: Project) {
+		new TimelineLite()
+			.to(this.elem, 0.5, { opacity: 0 })
+			.add(() => this.ngZone.run(() => (this._selectedProject = value)))
+			.to(this.elem, 0.5, { opacity: 1 }, "+=0.25");
+	}
 
 	public get skills(): string {
 		return this.selectedProject.skills.map(s => s.name).sort().join(", ");
 	}
+
+	private get elem(): HTMLElement {
+		return this._elem.nativeElement;
+	}
+
+	constructor(private _elem: ElementRef, private ngZone: NgZone) {}
 }
