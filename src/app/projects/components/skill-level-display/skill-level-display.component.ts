afff9c91fb5f2ec2e7d7b0a3240d0b6bb98f57d4
Philip Fulgham <philfulgham@gmail.com>
2019-03-06 22:04:48 -0500

    Build more sophisticated skills grid

---
+++ src/app/projects/components/skill-level-display/skill-level-display.component.ts
@@ -0,0 +1,51 @@
+import { Component, HostBinding, Input } from "@angular/core";
+
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+	selector: "skill-level-display",
+	templateUrl: "./skill-level-display.component.html",
+	styleUrls: ["./skill-level-display.component.scss"],
+})
+export class SkillLevelDisplayComponent {
+	@Input() level: number;
+	public starIcon = faStar;
+
+	public get levels() {
+		return [
+			{ i: 1, name: "Newbie" },
+			{ i: 2, name: "Learning" },
+			{ i: 3, name: "Familiar" },
+			{ i: 4, name: "Advanced" },
+			{ i: 5, name: "Pro" },
+		];
+	}
+
+	@HostBinding("class.level-0") private get isLevel0(): boolean {
+		return this.level === 0;
+	}
+
+	@HostBinding("class.level-1") private get isLevel1(): boolean {
+		return this.level === 1;
+	}
+
+	@HostBinding("class.level-2") private get isLevel2(): boolean {
+		return this.level === 2;
+	}
+
+	@HostBinding("class.level-3") private get isLevel3(): boolean {
+		return this.level === 3;
+	}
+
+	@HostBinding("class.level-4") private get isLevel4(): boolean {
+		return this.level === 4;
+	}
+
+	@HostBinding("class.level-5") private get isLevel5(): boolean {
+		return this.level === 5;
+	}
+
+	public isInLevel(star: number): boolean {
+		return star <= this.level;
+	}
+}
---
+++ src/app/projects/components/skills-grid-item/skills-grid-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+import { Skill } from "app/projects/models";
+
+@Component({
+	selector: "skills-grid-item",
+	templateUrl: "./skills-grid-item.component.html",
+	styleUrls: ["./skills-grid-item.component.scss"],
+})
+export class SkillsGridItemComponent {
+	@Input() skill: Skill;
+}
