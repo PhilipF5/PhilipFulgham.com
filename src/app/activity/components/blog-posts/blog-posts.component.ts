3976fada03bf756f01276e8332a73e85dfeab788
Philip Fulgham <philfulgham@gmail.com>
2019-03-10 22:30:07 -0400

    Clean up unused things left in BlogPostsComponent

--- src/app/activity/components/blog-posts/blog-posts.component.ts
+++ src/app/activity/components/blog-posts/blog-posts.component.ts
@@ -1,13 +1,11 @@
 import { Component } from "@angular/core";
 
-import { faMedium } from "@fortawesome/free-brands-svg-icons";
 import { Store, select } from "@ngrx/store";
 import { Observable } from "rxjs";
 
 import { BlogPostsRequested } from "app/activity/activity.actions";
 import { BlogPost } from "app/activity/models";
 import { getBlogPosts } from "app/activity/activity.selectors";
-import { BlogPostService } from "app/activity/services";
 
 import { environment } from "environments/environment";
 
@@ -20,7 +18,6 @@ export class BlogPostsComponent {
 	public blogUrl: string = environment.BLOG_URL;
 	public count: number;
 	public error: string;
-	public mediumIcon = faMedium;
 	public posts: Observable<BlogPost[]> = this.store.pipe(select(getBlogPosts));
 
 	public get hasError(): boolean {
