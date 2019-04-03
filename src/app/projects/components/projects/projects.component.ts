909076fae0f437a23799cdd81bea11e274d2ec7f
Philip Fulgham <philfulgham@gmail.com>
2019-04-02 21:02:12 -0400

    Add error state to projects section

--- src/app/projects/components/projects/projects.component.ts
+++ src/app/projects/components/projects/projects.component.ts
@@ -1,6 +1,9 @@
 import { Component } from "@angular/core";
 
+import { Store, select } from "@ngrx/store";
+
 import { Project } from "app/projects/models";
+import { getProjectsError } from "app/projects/selectors";
 
 @Component({
 	selector: "projects",
@@ -8,7 +11,10 @@ import { Project } from "app/projects/models";
 	styleUrls: ["./projects.component.scss"],
 })
 export class ProjectsComponent {
+	public error$ = this.store.pipe(select(getProjectsError));
 	public selectedProject: Project;
+
+	constructor(private store: Store<any>) {}
 
 	public changeSelectedProject(project: Project) {
 		this.selectedProject = project;
