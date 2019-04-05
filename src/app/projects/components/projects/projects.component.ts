8acebdb90988c3215fd456720924c4b5426210fa
Philip Fulgham <philfulgham@gmail.com>
2019-04-04 21:11:02 -0400

    Automatically scroll to project section when param is present

--- src/app/projects/components/projects/projects.component.ts
+++ src/app/projects/components/projects/projects.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 import { Store, select } from "@ngrx/store";
+import { filter, take, withLatestFrom } from "rxjs/operators";
 
 import { Project } from "app/projects/models";
-import { getProjectsError } from "app/projects/selectors";
+import { getProjects, getProjectsError } from "app/projects/selectors";
 
 @Component({
 	selector: "projects",
@@ -14,7 +16,30 @@ export class ProjectsComponent {
 	public error$ = this.store.pipe(select(getProjectsError));
 	public selectedProject: Project;
 
-	constructor(private store: Store<any>) {}
+	private projects$ = this.store.pipe(
+		select(getProjects),
+		filter(p => !!(p && p.length))
+	);
+
+	private get elem(): HTMLElement {
+		return this._elem.nativeElement;
+	}
+
+	constructor(private _elem: ElementRef, private route: ActivatedRoute, private store: Store<any>) {}
+
+	ngAfterViewInit() {
+		this.projects$
+			.pipe(
+				take(1),
+				withLatestFrom(this.route.queryParamMap)
+			)
+			.subscribe(([projects, paramMap]) => {
+				let projectId: string;
+				if ((projectId = paramMap.get("showProject"))) {
+					setTimeout(() => this.elem.scrollIntoView(), 1000);
+				}
+			});
+	}
 
 	public changeSelectedProject(project: Project) {
 		this.selectedProject = project;
